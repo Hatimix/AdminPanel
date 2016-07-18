81d75f0c33c926ab39e8d7a8a69ba7f979ff1967
Hatimix <PGeekC@gmail.com>
2016-07-18 16:37:44 +0100

    Test du commit leave out the class attribute

--- Pages/Templates/Default.php
+++ Pages/Templates/Default.php
@@ -30,7 +30,7 @@
     </div>
     <ul>
         <li class="active"><a href="#">Home</a></li>
-        <li><a class="glyphicon glyphicon-arrow-right" href="#">&nbsp;Link</a></li>
+        <li><a href="#">Articles</a></li>
         <li><a href="#">Link</a></li>
         <li><a href="#">Link</a></li>
         <li><a href="#">Link</a></li>
